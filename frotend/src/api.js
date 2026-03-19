f259737f858bb2ddc5ec4268c1eae51a8d8a0c27
Harshcha222 <rchaturvedi087@gmail.com>
2026-03-19 15:35:11 +0530

    frontend ready for deployment

--- frotend/src/api.js
+++ frotend/src/api.js
@@ -1,14 +1,32 @@
+// import axios from 'axios';
+
+// const API = 'http://localhost:5000'; // Change if backend runs elsewhere
+
+// export const api = axios.create({
+//   baseURL: API,
+// });
+
+// // Attach JWT to all requests
+// api.interceptors.request.use(config => {
+//   const token = localStorage.getItem('token');
+//   if (token) config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// });
+
+
 import axios from 'axios';
 
-const API = 'http://localhost:5000'; // Change if backend runs elsewhere
+const API = 'https://hr-system-mwrd.onrender.com';
 
 export const api = axios.create({
   baseURL: API,
 });
 
 // Attach JWT to all requests
-api.interceptors.request.use(config => {
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
